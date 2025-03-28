302774c61b4baf4b87768be40c011833b1f37964
lgonzalez1 <otfgonzalez@gmail.com>
2025-03-28 11:52:39 -0700

    Testing before_install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,11 @@
 
 echo "Install secrets"
 sudo yum update -y
+
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.11
+
+
+cd /home/ubuntu/app/scripts/deploy_env.py
